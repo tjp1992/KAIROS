f1d298464656d85636a81da04d4f12b9d47091c6
Parkhyun-jun <60635966+Parkhyun-jun@users.noreply.github.com>
2020-05-14 10:55:59 +0900

    test

--- ProjectKairos/src/admin/mypage/controller/AdminNoticeServlet.java
+++ ProjectKairos/src/admin/mypage/controller/AdminNoticeServlet.java
@@ -29,6 +29,7 @@ public class AdminNoticeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/adminMypage/adminNotice.jsp");
 		rd.forward(request, response);
 	}
