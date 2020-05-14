4571bcabe4266b653f21671fb5ae873449a55d53
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-05-14 10:00:46 +0900

    0514 Fix - SearchFrmServlet

--- ProjectKairos/src/search/controller/SearchFrmServlet.java
+++ ProjectKairos/src/search/controller/SearchFrmServlet.java
@@ -29,7 +29,7 @@ public class SearchFrmServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.getRequestDispatcher("/WEB-INF/views/search/search.jsp").forward(request, response);
+		request.getRequestDispatcher("/search").forward(request, response);
 		
 	}
 
